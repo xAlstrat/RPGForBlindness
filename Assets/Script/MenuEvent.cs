7e2ed4ca2818b02b4e2479d66c95cafcf4ad331a
xAlstrat <rodak.ro@gmail.com>
2015-11-02 13:15:27 -0300

    Se quitó SceneLoader de los menús.
    Se quitó SceneLoader puesto que no son necesarias la persistencia de
    objetos y las transiciones suaves en esas escenas.

--- Assets/Script/MenuEvent.cs
+++ Assets/Script/MenuEvent.cs
@@ -4,20 +4,24 @@ using System.Collections;
 public class MenuEvent : MonoBehaviour {
 
 	public void goMainMenu(){
-		SceneLoader.GetInstance ().load ("Welcome");
+		//SceneLoader.GetInstance ().load ("Welcome");
+		Application.LoadLevel("Welcome");
 	}
 
 	public void goLoadMenu(){
-		SceneLoader.GetInstance ().load ("Load");
+		//SceneLoader.GetInstance ().load ("Load");
+		Application.LoadLevel("Load");
 	}
 
 	public void startNewGame(){
 		ApplicationData.reset ();
 		ApplicationData.save ();
-		SceneLoader.GetInstance ().load ("HallState");
+		//SceneLoader.GetInstance ().load ("HallState");
+		Application.LoadLevel("HallState");
 	}
 
 	public void startLoadedGame(){
-		SceneLoader.GetInstance ().load ("HallState");
+		//SceneLoader.GetInstance ().load ("HallState");
+		Application.LoadLevel("HallState");
 	}
 }
