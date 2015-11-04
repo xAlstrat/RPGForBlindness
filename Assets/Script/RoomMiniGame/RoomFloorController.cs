bb0cc5689dc316bc1825304e6398938ea75dd2b4
Matías Pardo G <matias.pardo@ideauno.cl>
2015-11-04 04:08:59 -0300

    transformacion del cuarto aleatoria

--- Assets/Script/RoomMiniGame/RoomFloorController.cs
+++ Assets/Script/RoomMiniGame/RoomFloorController.cs
@@ -18,8 +18,116 @@ public class RoomFloorController : MonoBehaviour {
 
 	void StartGame ()
 	{
-		// Rotar el plano
-		transform.Rotate (0.0f, 180.0f, 0.0f);
+		// Rotar el plano 60º
+		// transform.Rotate (0.0f, 60.0f, 0.0f);
+		GenerateTransformation ();
 		started = true;
+	}
+
+	void GenerateTransformation()
+	{
+		// Elegir etre rotar, reflejar y trasladar
+		int caseSwitch = Random.Range(0,3); // creates a number between 0 and 2
+		switch (caseSwitch)
+		{
+		case 0:
+			// Rotar
+			// Izquierda o Derecha
+			int direction_selector = Random.Range(0,2); // numero entre 0 y 1
+			int direction;
+			switch (direction_selector)
+			{
+			case 0:
+				//Izquierda
+				direction = 1;
+				break;
+			default:
+				//Derecha
+				direction = -1;
+				break;
+			}
+			// Grados
+			float angle;
+			int angle_selector = Random.Range (0,3); // numero entre 0 y 2
+			// Transformacion
+			switch (angle_selector)
+			{
+			case 0:
+				angle = 45.0f;
+				break;
+			case 1:
+				angle = 90.0f;
+				break;
+			default:
+				angle = 135.0f;
+				break;
+			}
+			// Rotar
+			transform.Rotate (0.0f, angle * direction, 0.0f);
+			break;
+		case 1:
+			// Trasladar
+			// Horizontal o Vertical
+			int axis_t_selector = Random.Range (0, 2); // 0 o 1
+			int side_selector = Random.Range (0, 2); // 0 o 1
+			Vector3 old_t_position = transform.position;
+			float old_t_x = old_t_position.x;
+			float old_t_y = old_t_position.y;
+			float old_t_z = old_t_position.z;
+
+			switch (axis_t_selector)
+			{
+			case 0:
+				// Horizontal
+				switch (side_selector)
+				{
+				case 0:
+					//Izquierda
+					transform.position = new Vector3(old_t_x + 5, old_t_y, old_t_z);
+					break;
+				default:
+					// Derecha
+					transform.position = new Vector3(old_t_x - 7, old_t_y, old_t_z);
+					break;
+				}
+				break;
+			default:
+				// Vertical
+				switch (side_selector)
+				{
+				case 0:
+					// Arriba
+					transform.position = new Vector3(old_t_x, old_t_y, old_t_z - 3);
+					break;
+				default:
+					// Abajo
+					transform.position = new Vector3(old_t_x, old_t_y, old_t_z + 3);
+					break;
+				}
+				break;
+			}
+			break;
+		default:
+			// Reflejar
+			// Horizontal o Verticalmente
+			int axis_selector = Random.Range (0, 2); // 0 o 1
+			Vector3 old_position = GameObject.FindGameObjectWithTag("Endblock").transform.position;
+			float old_x = old_position.x;
+			float old_y = old_position.y;
+			float old_z = old_position.z;
+			switch (axis_selector)
+			{
+			case 0:
+				// Vertical
+				transform.Rotate (0.0f, 180.0f, 0.0f);
+				GameObject.FindGameObjectWithTag("Endblock").transform.position = new Vector3( - old_x + 6, old_y, old_z);
+				break;
+			default:
+				// Horizontal
+				GameObject.FindGameObjectWithTag("Endblock").transform.position = new Vector3(old_x, old_y, - old_z);
+				break;
+			}
+			break;
+		}
 	}
 }
